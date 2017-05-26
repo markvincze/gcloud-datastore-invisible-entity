d24721b253b666c1be4c092b8e4cd7782b4d985a
Mark Vincze <mvincze@travix.com>
2017-05-26 13:42:37 +0200

    Add some test code illustrating the invisible entity issue

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # gcloud-datastore-invisible-entity
 Some test code illustrating how we sometimes cannot retrieve commited entities from Google Datastore.
+
+## Usage
+Edit the values of the `gcloudProject` and `gcloudDatastoreNamespace` fields to contain your Gcloud project name and the namespace you want to use, then simply build and run the project from the `/src/GcloudDatastoreInvisibleEntity` folder.
+
+```
+dotnet restore
+dotnet run
+```
+
+In the output you'll see a warning if the commited entity could not be read with a query afterwards. An example output looks like this.
+
+```
+Iteration 0 done.
+Iteration 1 done.
+-------------------------------------------------
+WARNING: Version was not found after insertion!!!
+-------------------------------------------------
+Iteration 2 done.
+Iteration 3 done.
+Iteration 4 done.
+Iteration 5 done.
+-------------------------------------------------
+WARNING: Version was not found after insertion!!!
+-------------------------------------------------
+Iteration 6 done.
+Iteration 7 done.
+Iteration 8 done.
+Iteration 9 done.
+Iteration 10 done.
+Iteration 11 done.
+Iteration 12 done.
+Iteration 13 done.
+Iteration 14 done.
+Iteration 15 done.
+Iteration 16 done.
+Iteration 17 done.
+Iteration 18 done.
+Iteration 19 done.
+```
---
+++ src/GcloudDatastoreInvisibleEntity/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Cloud.Datastore.V1;
+
+namespace GcloudDatastoreInvisibleEntity
+{
+    class Program
+    {
+        private const string gcloudProject = "my-gcloud-project";
+        private const string gcloudDatastoreNamespace = "invisible-entity-test";
+
+        private const string entityKind = "TestEntity";
+        private const string entityKey = "EntityKey";
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                Cleanup();
+                DoTest().Wait();
+                Console.WriteLine("Iteration {0} done.", i);
+            }
+        }
+
+        private static void Cleanup()
+        {
+            var db = DatastoreDb.Create(gcloudProject, gcloudDatastoreNamespace);
+
+            bool done = false;
+
+            while (!done)
+            {
+                Query query = new Query(entityKind);
+
+                var versions = db.RunQuery(query);
+
+                // NOTE Google Datastore doesn't allow to delete more than 500 entities in one operation.
+                db.Delete(versions.Entities.Take(500).ToList());
+
+                done = versions.Entities.Count <= 500;
+            }
+
+        }
+        
+        private static async Task DoTest()
+        {
+            var datastoreDb = DatastoreDb.Create(gcloudProject, gcloudDatastoreNamespace);
+            
+            using(var tran = await datastoreDb.BeginTransactionAsync())
+            {
+                // Insert a test entity.
+                var keyStr = Guid.NewGuid().ToString();
+                Key key = datastoreDb.CreateKeyFactory(entityKind).CreateKey(keyStr);
+
+                var entity = new Entity
+                {
+                    Key = key,
+                    ["bar"] = "foo"
+                };
+                
+                tran.Upsert(entity);
+                await tran.CommitAsync();
+                
+                // Entity inserted, check if we can read it.
+                var query = new Query(entityKind);
+                
+                var entities = (await datastoreDb.RunQueryAsync(query)).Entities;
+
+                if (!entities.Any(e => e.Key.Path.Last().Name == keyStr))
+                {
+                    Console.WriteLine("-------------------------------------------------");
+                    Console.WriteLine("WARNING: Version was not found after insertion!!!");
+                    Console.WriteLine("-------------------------------------------------");
+                }
+            }
+        }
+    }
+}
